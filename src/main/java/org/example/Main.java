dbbc40f5edb2aa3a21a94b88dd218a58671148f9
susicharis <haris.susic@stu.ibu.edu.ba>
2024-10-07 16:25:27 +0200

    Lastly

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,25 +4,29 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    // First Task
     public static void main(String[] args) {
-
         String correctPassword = "java123";
-
 
         Scanner scanner = new Scanner(System.in);
 
-
         System.out.print("Unesite lozinku: ");
         String enteredPassword = scanner.nextLine();
-
 
         if (enteredPassword.equals(correctPassword)) {
             System.out.println("Tačna lozinka! Evo tajne poruke:");
             System.out.println("Tajna poruka: Dobrodošli u svijet programiranja u Javi!");
         } else {
-            System.out.println("Pogrešna lozinka. Jedan Dva Tri ");
+            System.out.println("Pogrešna lozinka. Try Again! ");
         }
 
-
         scanner.close();
+    }
+
+
+    // Second Task
+
+
+
 }
